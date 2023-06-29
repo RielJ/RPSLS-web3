363766131aa3768408e82e8b7f6ccdb62e9d06fa
RielJ <bulaybulay.rielj@gmail.com>
2023-06-30 01:26:02 +0800

    build fixes

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,29 +1,8 @@
-"use client";
-import { useState, useEffect } from "react";
 import { Main, Navbar } from "./sections";
-import { CursorEffects } from "@/components";
 
 export function Page() {
-  const [width, setWidth] = useState<number>(0);
-
-  // TODO: Determine how to disable CursorEffects when
-  // it's on touch only devices.
-  function handleWindowSizeChange() {
-    setWidth(window.innerWidth);
-  }
-
-  useEffect(() => {
-    window.addEventListener("resize", handleWindowSizeChange);
-    setWidth(window.innerWidth);
-    return () => {
-      window.removeEventListener("resize", handleWindowSizeChange);
-    };
-  }, []);
-
-  const isMobile = width <= 768;
   return (
     <>
-      {!isMobile && <CursorEffects />}
       <main className="container relative w-full min-h-screen flex flex-col justify-center items-center z-10">
         <Navbar />
         <Main />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
+"use client";
 import "@rainbow-me/rainbowkit/styles.css";
 import "./globals.css";
 import { Poppins } from "next/font/google";
 import { Providers } from "./providers";
 import { ThemeProvider } from "./theme-provider";
-import { Toaster } from "@/components";
+import { CursorEffects, Toaster } from "@/components";
+import { useState, useEffect } from "react";
 
-export const metadata = {
-  title: "Kleros RPS",
-};
+// export const metadata = {
+//   title: "Kleros RPS",
+// };
 
 const poppins = Poppins({
   weight: "700",
@@ -20,6 +22,24 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [width, setWidth] = useState<number>(0);
+
+  // TODO: Determine how to disable CursorEffects when
+  // it's on touch only devices.
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    setWidth(window.innerWidth);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  const isMobile = width <= 768;
+
   return (
     <html lang="en">
       <body
@@ -27,6 +47,7 @@ export default function RootLayout({
       >
         <Providers>
           <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+            {!isMobile && <CursorEffects />}
             {children}
             <Toaster />
           </ThemeProvider>
